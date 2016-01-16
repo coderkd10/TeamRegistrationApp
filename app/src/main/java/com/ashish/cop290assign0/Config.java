c03d7b37a05a640eb5d04de49d483f1ae680f63f
Abhishek Kedia <kedia.abhishek10@gmail.com>
2016-01-16 14:46:24 +0530

    readd entry number box max length to 11. was removed by merge 49977b1.

--- app/src/main/java/com/ashish/cop290assign0/Config.java
+++ app/src/main/java/com/ashish/cop290assign0/Config.java
@@ -7,5 +7,5 @@ public class Config {
     final static String SERVER_URL = "http://agni.iitd.ernet.in/cop290/assign0/register/";
     final static String LDAP_BASE_URL = "http://10.7.172.112:80/search/ldap1.php";
     final static String REGEX_NAME = "^[\\p{L} .'-]+$";
-    final static String REGEX_ENTRY_NUMBER ="(\\d{4})((?:[a-zA-Z][a-zA-Z]\\d))(\\d{4})";
+    final static String REGEX_ENTRY_NUMBER ="(\\d{4})((?:[a-zA-Z]{2}[a-zA-z\\d]))(\\d{4})";
 }
