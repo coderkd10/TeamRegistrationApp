cf7bb028f0b34a212a6c03eb4930de155f655560
Abhishek Kedia <kedia.abhishek10@gmail.com>
2016-01-24 03:32:02 +0530

    remove menu and centre welcome activity.

--- app/src/main/java/com/ashish/cop290assign0/WelcomeActivity.java
+++ app/src/main/java/com/ashish/cop290assign0/WelcomeActivity.java
@@ -33,27 +33,6 @@ public class WelcomeActivity extends ActionBarActivity {
         //iconLayoutMain.animate().scaleY(1);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     public void proceed(View v){
         Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
         startActivity(intent);
